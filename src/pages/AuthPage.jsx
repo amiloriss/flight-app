fa55f1e95cd245d436dae131afa1562e801c5e5e
georgiy <33293661+amiloriss@users.noreply.github.com>
2021-01-17 13:17:02 +0300

    implement pages

--- src/pages/AuthPage.jsx
+++ src/pages/AuthPage.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import AuthForm from '../components/AuthForm';
+import backgroundImage from '../images/backgroundImage.jpg';
 
 const AuthPage = () => {
-	return <div></div>;
+	return (
+		<div className='auth-page'>
+			<img src={backgroundImage} alt='background' />
+			<AuthForm />
+		</div>
+	);
 };
 
 export default AuthPage;
--- src/pages/FlightPage.jsx
+++ src/pages/FlightPage.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import FlightWindow from '../components/FlightWindow';
 
 const FlightPage = () => {
-	return <div></div>;
+	return (
+		<div className='flight-page'>
+			<FlightWindow />
+		</div>
+	);
 };
 
 export default FlightPage;
