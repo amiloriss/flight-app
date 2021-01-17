9508090703ecd7f86fcd9c79b286a9dc66b808f0
georgiy <33293661+amiloriss@users.noreply.github.com>
2021-01-17 13:16:25 +0300

    add routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import './styles/style.scss';
+
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import AuthPage from './pages/AuthPage';
+import FlightPage from './pages/FlightPage';
 
 const App = () => {
-	return <div></div>;
+	return (
+		<Router>
+			<div className='app'>
+				<Route exact path='/' render={() => <AuthPage />} />
+				<Route exact path='/home' render={() => <FlightPage />} />
+			</div>
+		</Router>
+	);
 };
 
 export default App;
